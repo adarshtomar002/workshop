286c243153f913716c140b8f4197ed1bc31443ad
adarshtomar002 <adarsh00421202018@msi-ggsip.org>
2019-09-07 12:23:56 +0530

    firstchange

---
+++ intro.cpp
@@ -0,0 +1,14 @@
+#include<iostream.h>
+#include<conio.h>
+
+using namespace std;
+
+int main() {
+    int a=10;
+    cout<<"learning Git and Github"<<endl;
+    cout<<"Basics of Git and Gituhubs";
+    cout<<a;
+
+    //This is our first version..
+    return 0;
+}
